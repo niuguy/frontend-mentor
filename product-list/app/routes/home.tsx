03141bfd13a8ec784d4c740c0c9f0bd3b8748e45
niuguy <tf.wang.seu@gmail.com>
2025-02-25 07:07:06 +0000

    style(home.tsx): change div to main for semantic HTML improvement in the Home component

--- product-list/app/routes/home.tsx
+++ product-list/app/routes/home.tsx
@@ -11,7 +11,7 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return (
-    <div className="max-w-3xl mx-auto p-4 flex flex-col lg:flex-row gap-4">
+    <main className="max-w-3xl mx-auto p-4 flex flex-col lg:flex-row gap-4">
       <div className="flex-1 flex flex-col gap-4">
         <h1 className="text-3xl font-bold mb-6">Desserts</h1>
         <div>
@@ -21,6 +21,6 @@ export default function Home() {
       <div className="w-full lg:w-64">
         <ShoppingCart />
       </div>
-    </div>
+    </main>
   );
 }
